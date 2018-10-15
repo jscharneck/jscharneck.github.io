7a22d6806dd5a6123fab16775ef93446a0f1739a
Jose <jose.scharneck87@gmail.com>
2018-10-15 10:23:31 +0200

    updated

--- downloads/GetSerialNumer and PCModel.bat
+++
@@ -1,7 +0,0 @@
-wmic bios get serialnumber  > SerialNumberAndPCModel.txt
-^
-wmic computersystem get manufacturer >> SerialNumberAndPCModel.txt 
-^
-wmic computersystem get model >> SerialNumberAndPCModel.txt
-pause
-start SerialNumberAndPCModel.txt
